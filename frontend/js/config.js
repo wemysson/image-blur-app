81aa78cc1144e4d1a52ebef919ca0bf616913b56
Wemyson <Wemysson.wr@hotmail.com>
2025-01-28 13:35:48 -0300

    Atualizando URL do backend para o Render

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,2 @@
 // Configuração da API
-const API_URL = 'https://seu-backend.herokuapp.com'; // Você vai precisar atualizar isso com a URL do seu backend
+const API_URL = 'https://image-blur-app-backend.onrender.com'; // Você vai precisar atualizar isso com a URL do seu backend
